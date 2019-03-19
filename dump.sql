74f92d9bab81006f19d72e872b34c01ee6d469be
serhiyosetskiy <serhiyosetskiy@qbex.io>
2019-03-19 11:11:06 +0200

    dump sql

---
+++ dump.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost:3306
+-- Время создания: Мар 19 2019 г., 11:10
+-- Версия сервера: 5.7.22-0ubuntu0.17.10.1
+-- Версия PHP: 5.6.36-1+ubuntu17.10.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `qbc_task`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `manuals`
+--
+
+CREATE TABLE `manuals` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `type_field` enum('select','text','textarea','file') NOT NULL DEFAULT 'select',
+  `position` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `manual_options`
+--
+
+CREATE TABLE `manual_options` (
+  `id` int(11) NOT NULL,
+  `manual_id` int(11) NOT NULL,
+  `label` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `manual_types`
+--
+
+CREATE TABLE `manual_types` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `type_key` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `manual_values`
+--
+
+CREATE TABLE `manual_values` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `manual_id` int(11) NOT NULL,
+  `value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `info` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `manuals`
+--
+ALTER TABLE `manuals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `manual_options`
+--
+ALTER TABLE `manual_options`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `manual_types`
+--
+ALTER TABLE `manual_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `manual_values`
+--
+ALTER TABLE `manual_values`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `manuals`
+--
+ALTER TABLE `manuals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT для таблицы `manual_options`
+--
+ALTER TABLE `manual_options`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT для таблицы `manual_types`
+--
+ALTER TABLE `manual_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `manual_values`
+--
+ALTER TABLE `manual_values`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
