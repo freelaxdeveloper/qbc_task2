32fb043f85b8a41b6ba45eb917ca14017864a5e4
serhiyosetskiy <serhiyosetskiy@qbex.io>
2019-03-25 09:55:18 +0200

    update routes

--- app/routes.php
+++ app/routes.php
@@ -2,12 +2,16 @@
 
 $r->get('/', 'HomeController@index');
 
-// {id} must be a number (\d+)
-$r->get('/pages/people/{id:\d+}', 'UserController@show');
-$r->get('/pages/people/{id:\d+}/edit', 'UserController@edit');
-$r->post('/pages/people/{id:\d+}/edit', 'UserController@update');
+$r->addGroup('/pages/people', function (FastRoute\RouteCollector $r) {
+  // {id} must be a number (\d+)
+  $r->get('/{id:\d+}', 'UserController@show');
+  $r->get('/{id:\d+}/edit', 'UserController@edit');
+  $r->post('/{id:\d+}/edit', 'UserController@update');
+});
 
 $r->get('/manual/value/{user_id:\d+}/{value_id:\d+}/delete', 'ManualValueController@delete');
 
 // The /{title} suffix is optional
 $r->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'get_article_handler');
+
+// https://github.com/nikic/FastRoute
